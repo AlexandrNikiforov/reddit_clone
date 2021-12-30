5918e4a8ba57d4bada48625dbcf5ec11397464cb
alexandr.nikiforov <nikiforov.san.sanich@gmail.com>
2021-12-30 10:32:19 +0300

    Implement a voting mechanism

--- src/main/java/com/vega/springit/domain/Vote.java
+++ src/main/java/com/vega/springit/domain/Vote.java
@@ -2,32 +2,32 @@ package com.vega.springit.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 @Entity
-//@Table(name = "vote", schema = "springit")
 public class Vote extends Auditable {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    private int vote;
+    @NonNull
+    private short direction;
 
-    @Override
-    public String toString() {
-        return "Vote{" +
-                "id=" + id +
-                ", vote=" + vote +
-                '}';
-    }
+    @NonNull
+    @ManyToOne
+    private Link link;
 
 }
